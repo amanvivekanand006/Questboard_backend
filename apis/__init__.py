6f42bd3d13bde74b713b97ea33c5c341ba16e744
v aman <amanamn2000@gmail.com>
2025-03-20 02:44:20 +0530

    userregistration

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,8 +1,10 @@
 import fastapi
 from pymongo import MongoClient
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel,EmailStr,Field
+from typing import Optional,List
 from fastapi.routing import APIRouter
 import uuid
+
+api_router = APIRouter()
 
 mongo_uri = "mongodb+srv://amanvivekanand:aman994909@cluster0.nszektx.mongodb.net/"
--- apis/admin.py
+++ apis/admin.py
@@ -3,6 +3,7 @@ from . import *
 client = MongoClient(mongo_uri)
 db = client["questboard"]
 admins_collection = db["admins"]
+user_collection = db.usercollection
 
 class admin(BaseModel):
     name : str
@@ -10,3 +11,23 @@ class admin(BaseModel):
     phone_number : int
     is_admin : bool
     admin_id : int
+
+class user_registration(BaseModel):
+    user_id : str
+    username: str
+    email: EmailStr
+    profile_pic: str
+    xp : int = Field(default = 0)
+    coins : int
+    level : int
+    badges : List[str] = []
+
+@api_router.post("/user_registration", operation_id="creating_user_account")
+def create_user(userschema: user_registration):
+    response = userschema.dict()
+    result = user_collection.insert_one(response)  # result is an InsertOneResult
+    user_id = str(result.inserted_id)  # Convert the inserted_id to a string
+    return {"message": "User created successfully", "user_id": user_id}
+
+
+
