2eb93539dc6dda3a0ece55a054f83f6375389400
Ruthvik <ruthviksayabugari@gmail.com>
2025-03-20 12:02:11 +0530

    updated

--- apis/admin.py
+++ apis/admin.py
@@ -36,5 +36,40 @@ def create_user(userschema: user_registration):
     user_id = str(result.inserted_id)  # Convert the inserted_id to a string
     return {"message": "User created successfully", "user_id": user_id}
 
+@api_router.post("/create_admin")
+async def create_admin(admin: createadmin):
+    admin_dict = admin.dict() 
+    admin_dict["admin_id"] = str(uuid.uuid4())
+
+    result = admins_collection.insert_one(admin_dict)
+    if result.inserted_id:
+       admin_dict.pop("_id", None)
+       return admin_dict
+    raise HTTPException(status_code=500, detail="Failed to create admin")
 
 
+@api_router.get("/get_admin")
+async def get_admin(admin_id : str):
+    Admin = admins_collection.find_one({"admin_id": admin_id})
+    if not Admin:
+        raise HTTPException(status_code=404, detail="Admin not found")
+    Admin.pop("_id", None)
+    return Admin
+
+@api_router.put("/update_admin")
+async def update_admin(admin_id: str, admin : createadmin):
+    updated_admin = admin.dict()
+    result = admins_collection.update_one({"admin_id":admin_id},{"$set":updated_admin})
+    
+    if result.matched_count == 0:
+        raise HTTPException(status_code=404, detail="Admin not found")
+    updated_admin["admin_id"] = admin_id
+    return updated_admin
+
+@api_router.delete("/delete_admin")
+async def delete_admin(admin_id:str):
+    result= admins_collection.delete_one({"admin_id":admin_id})
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Admin not found")
+    return {"message":"admin deleted successfully"}
+    
