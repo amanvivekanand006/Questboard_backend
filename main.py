1b2584cef06093bfc78c760359ef2a3b25689a52
v aman <amanamn2000@gmail.com>
2025-03-22 14:23:46 +0530

    Quest api

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from apis import *
 app = FastAPI()
 
 from apis.admin import api_router as admin_router
+from apis.quests import api_router as quest_router
 
 app.include_router(admin_router)
+app.include_router(quest_router)
 
 
 app.add_middleware(
